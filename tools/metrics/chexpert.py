958321075308e812652d679550b2fb7da8d4d3f3
Brian Zhao <briannlongzhao@gmail.com>
2024-05-26 11:51:51 -0700

    update chexpert metrics

--- tools/metrics/chexpert.py
+++ tools/metrics/chexpert.py
@@ -6,8 +6,71 @@ import pandas as pd
 import time
 import torch
 
-from .eval_metrics import calculate_ce_metrics_mp, calculate_nlg_metrics
 
+def calculate_nlg_metrics(preds, gts):
+    # BLEU, METEOR, ROUGE-L, Perplexity
+    print("Calculating NLG metrics")
+    results = {}
+    bleu = evaluate.load("bleu")
+    gts_bleu = [[gt] for gt in gts]
+    for n in range(1, 5):
+        result = bleu.compute(predictions=preds, references=gts_bleu, max_order=n)
+        results[f"BLEU@{n}"] = result["bleu"]
+    meteor = evaluate.load("meteor")
+    result = meteor.compute(predictions=preds, references=gts)
+    results["METEOR"] = result["meteor"]
+    rouge = evaluate.load("rouge")
+    result = rouge.compute(predictions=preds, references=gts)
+    results["ROUGE_L"] = result["rougeL"]
+    perplexity = evaluate.load("perplexity", module_type="metric")
+    # result = perplexity.compute(predictions=preds, model_id="gpt2")
+    # results["Perplexity"] = result["mean_perplexity"]
+    return results
+
+def calculate_ce_metrics(preds, gts):
+    # P,R, F1
+    print("Calculating CE metrics")
+    labeler = ChexpertLabeler()
+    preds_labels, gts_labels = [], []
+    for pred, gt in tqdm(zip(preds, gts), total=len(preds)):
+        pred_label = labeler.get_label(pred)
+        gt_label = labeler.get_label(gt)
+        preds_labels.append([1 if pred_label[k] == 1 else 0 for k in CATEGORIES])
+        gts_labels.append([1 if gt_label[k] == 1 else 0 for k in CATEGORIES])
+    precision, recall, f1_score, _ = precision_recall_fscore_support(gts_labels, preds_labels, average="micro")
+    result = {"PRECISION": precision, "RECALL": recall, "F1_SCORE": f1_score}
+    return result
+
+
+def get_label(report_list):
+    labeler = ChexpertLabeler()
+    label_list = []
+    for report in tqdm(report_list):
+        label = labeler.get_label(report)
+        label = [1 if label[k] == 1 else 0 for k in CATEGORIES]
+        label_list.append(label)
+    del labeler
+    return np.array(label_list)
+
+
+def calculate_ce_metrics_mp(preds, gts, num_processes=os.cpu_count()-1):
+    # P,R, F1
+    print("Calculating CE metrics")
+    # preds = [clean_report(pred) for pred in preds]
+    # gts = [clean_report(gt) for gt in gts]
+    preds_gts = preds + gts
+    num_processes = min(num_processes, len(preds_gts))
+    preds_gts_chunks = list(batched(preds_gts, math.ceil(len(preds_gts)/num_processes)))
+    if num_processes != len(preds_gts_chunks): num_processes = len(preds_gts_chunks)
+    pool = Pool(num_processes)
+    preds_gts_labels = pool.imap(get_label, preds_gts_chunks)
+    pool.close()
+    pool.join()
+    preds_gts_labels = np.concatenate(list(preds_gts_labels))
+    preds_labels, gts_labels = preds_gts_labels[:len(preds)], preds_gts_labels[len(preds):]
+    precision, recall, f1_score, _ = precision_recall_fscore_support(gts_labels, preds_labels, average="micro")
+    result = {"PRECISION": precision, "RECALL": recall, "F1_SCORE": f1_score}
+    return result
 
 
 class CheXpertMetrics(NaturalLanguage):
